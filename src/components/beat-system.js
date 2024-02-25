cdaf2f6a5810dde9587199f58e252c7489233459
F.Buldo <frbuldo@gmail.com>
2024-02-25 01:20:45 +0100

    Modify beat-system and beat components to work with the new hitdetection and scoring system. Also move beat-system i a dedicated file

---
+++ src/components/beat-system.js
@@ -0,0 +1,267 @@
+import { BladeHitDetector, PunchHitDetector } from './HitDetector.js';
+import { PUNCH_OFFSET, SWORD_OFFSET } from './beat-generator';
+
+// Play sound and explode at reach to test sync.
+const SYNC_TEST = !!AFRAME.utils.getUrlParameter('synctest');
+
+
+
+const WEAPON_COLORS = { right: 'blue', left: 'red' };
+
+const CLASSIC = 'classic';
+const PUNCH = 'punch';
+const RIDE = 'ride';
+
+const SIZES = {
+    [CLASSIC]: 0.48,
+    [PUNCH]: 0.35,
+    [RIDE]: 0.4
+};
+
+AFRAME.registerComponent('beat-system', {
+    schema: {
+        gameMode: { default: 'classic', oneOf: ['classic', 'punch', 'ride'] },
+        hasVR: { default: false },
+        inVR: { default: false },
+        isLoading: { default: false },
+        isPlaying: { default: false }
+    },
+
+    init: function () {
+        this.beats = [];
+        this.beatsToCheck = [];
+        this.blades = [];
+        this.fists = [];
+        this.weapons = null;
+        this.superCutIdx = 0;
+        this.superCuts = document.querySelectorAll('.superCutFx');
+        this.bladeEls = this.el.sceneEl.querySelectorAll('a-entity[blade]');
+        this.curveFollowRig = document.getElementById('curveFollowRig');
+        this.punchEls = this.el.sceneEl.querySelectorAll('a-entity[punch]');
+        this.curveEl = document.getElementById('curve');
+        this.size = SIZES[this.data.gameMode];
+        this.supercurveFollow = null;
+        this.beatEventsQueue = [];
+    },
+
+    play: function () {
+        for (let i = 0; i < 2; i++) {
+            this.blades.push(this.bladeEls[i].components.blade);
+            this.fists.push(this.punchEls[i].components.punch);
+        }
+
+        this.supercurve = this.curveEl.components.supercurve;
+        this.supercurveFollow = this.curveFollowRig.components['supercurve-follow'];
+    },
+
+    update: function (oldData) {
+        this.size = SIZES[this.data.gameMode];
+
+        if (oldData.isLoading && !this.data.isLoading) {
+            this.updateBeatPositioning();
+            this.weaponOffset = this.data.gameMode === CLASSIC ? SWORD_OFFSET : PUNCH_OFFSET;
+            this.weaponOffset = this.weaponOffset * 1.5 / this.supercurve.curve.getLength();
+        }
+
+        if (oldData.gameMode !== this.data.gameMode) {
+            this.weapons = this.data.gameMode === CLASSIC ? this.blades : this.fists;
+        }
+    },
+
+    tick: function (t, dt) {
+        // emit beat hits events
+        // Delay these events into next frame to spread out the workload.
+        if (this.beatEventsQueue.length > 0) {
+            const evData = this.beatEventsQueue.shift();
+            if (evData.good) {
+                this.el.sceneEl.emit('beathit', evData, true);
+            } else
+                this.el.sceneEl.emit('beatwrong', evData, true);
+        }
+        // check for collisions
+        if (!this.data.isPlaying || this.data.gameMode === RIDE) { return; }
+
+        const beatsToCheck = this.beatsToCheck;
+        const curve = this.supercurve.curve;
+        const progress = this.supercurveFollow.songProgress;
+
+        // Filter for beats that should be checked for collisions.
+        beatsToCheck.length = 0;
+        for (let i = 0; i < this.beats.length; i++) {
+            const beat = this.beats[i];
+
+            // Check if past the camera to return to pool.
+            const returnDistance = beat.isMine() ? 0.25 : 1.25;
+            if ((beat.el.object3D.position.z - returnDistance) > this.curveFollowRig.object3D.position.z) {
+                beat.returnToPool();
+                if (!beat.isMine())
+                    this.el.sceneEl.emit('beatmiss', null, true);
+                continue;
+            }
+
+            // Check beat is not already destroyed.
+            if (beat.destroyed) { continue; }
+
+            // Check if beat is close enough to be hit.
+            const beatProgress = beat.songPosition - this.weaponOffset;
+            if (progress < beatProgress) { continue; }
+
+
+            // Check if beat should be filtered out due to not being in front.
+            let inFront = true;
+            for (let i = 0; i < beatsToCheck.length; i++) {
+                if (beat.horizontalPosition === beatsToCheck[i].horizontalPosition &&
+                    beat.verticalPosition === beatsToCheck[i].verticalPosition &&
+                    beat.songPosition > beatsToCheck[i].songPosition) {
+                    inFront = false;
+                }
+                if (!inFront) { break; }
+            }
+            if (inFront) {
+                beatsToCheck.push(beat);
+            }
+
+        }
+
+        // Update bounding boxes and velocities.
+        this.weapons[0].tickBeatSystem(t, dt);
+        this.weapons[1].tickBeatSystem(t, dt);
+
+        // No beats to check means to collision to check.
+        if (!beatsToCheck.length) { return; }
+
+        // Check hits.
+        for (let i = 0; i < beatsToCheck.length; i++) {
+            // If ?synctest=true, auto-explode beat and play sound to easily test sync.
+            if ((SYNC_TEST || !this.data.hasVR) && !beatsToCheck[i].isMine()) { // if (false) { //
+                beatsToCheck[i].destroyBeat(this.weapons[0].el, Math.random() < 0.9);
+                beatsToCheck[i].el.parentNode.components['beat-hit-sound'].playSound(beatsToCheck[i].el, beatsToCheck[i].cutDirection);
+
+            } else {
+                for (let j = 0; j < beatsToCheck[i].hitDetectors.length; j++) {
+                    const hitDetector = beatsToCheck[i].hitDetectors[j];
+                    if (hitDetector.IsHit(t)) {
+                        this.processBeatHit(beatsToCheck[i], hitDetector)
+                        break;
+                    }
+                }
+            }
+        }
+
+
+    },
+
+    processBeatHit: function (beat, hitDetector) {
+        const data = beat.data;
+        const weaponEl = hitDetector.blade.el;
+        // if mine
+        if (beat.isMine()) {
+            beat.destroyBeat(weaponEl, false);
+            this.el.sceneEl.emit('minehit', null, true);
+            return;
+        }
+
+        // if it is a note
+        if (hitDetector.HitIsGood) {
+            // Haptics only for good hits.
+            try {
+                weaponEl.components.haptics__beat.pulse();
+            } catch (err) {
+                console.log(err);
+            }
+
+            beat.el.parentNode.components['beat-hit-sound'].playSound(beat.el, beat.cutDirection);
+            const scoreData = hitDetector.score;
+
+            // Super FX.
+            if (scoreData.percent >= 98) {
+                this.superCuts[this.superCutIdx].components.supercutfx.createSuperCut(
+                    beat.el.object3D, beat.data.color);
+                this.superCutIdx = (this.superCutIdx + 1) % this.superCuts.length;
+            }
+            scoreData.good = true;
+            this.beatEventsQueue.push(scoreData);
+        } else {
+            this.beatEventsQueue.push({ good: false });
+            beat.wrongHit();
+        }
+        beat.destroyBeat(weaponEl, hitDetector.HitIsGood);
+    },
+
+
+
+    horizontalPositions: {},
+
+    verticalPositions: {},
+
+    /**
+     * Update positioning between blocks, vertically and horizontally depending on
+     * game mode, and the height of the user.
+     *
+     * Adjustment revolves primary around SIZES, and the hMargin multiply factor.
+     */
+    updateBeatPositioning: (function () {
+        // Have punches be higher.
+        const BOTTOM_HEIGHTS = {
+            [CLASSIC]: 0.95,
+            [RIDE]: 0.95,
+            [PUNCH]: 1.20
+        };
+
+        const BOTTOM_HEIGHT_MIN = 0.4;
+        const REFERENCE_HEIGHT = 1.6;
+
+        return function () {
+            const gameMode = this.data.gameMode;
+            const horizontalPositions = this.horizontalPositions;
+            const verticalPositions = this.verticalPositions;
+
+            const heightOffset = this.el.sceneEl.camera.el.object3D.position.y - REFERENCE_HEIGHT;
+            const size = SIZES[gameMode];
+
+            // Horizontal margin based on size of blocks so they don't overlap, which a smidge
+            // of extra margin.
+            // For punch mode, we want a wider horizontal spread in punch range, but not vertical.
+            const hMargin = gameMode === CLASSIC ? size : size * 1.2;
+            horizontalPositions.left = -1.5 * hMargin;
+            horizontalPositions.middleleft = -0.5 * hMargin;
+            horizontalPositions.middle = hMargin;
+            horizontalPositions.middleright = 0.5 * hMargin;
+            horizontalPositions.right = 1.5 * hMargin;
+
+            // Vertical margin based on size of blocks so they don't overlap.
+            // And then overall shifted up and down based on user height (camera Y).
+            // But not too low to go underneath the ground.
+            const bottomHeight = BOTTOM_HEIGHTS[gameMode];
+            const vMargin = size;
+            verticalPositions.bottom = Math.max(
+                BOTTOM_HEIGHT_MIN,
+                bottomHeight + heightOffset);
+            verticalPositions.middle = Math.max(
+                BOTTOM_HEIGHT_MIN + vMargin,
+                bottomHeight + vMargin + heightOffset);
+            verticalPositions.top = Math.max(
+                BOTTOM_HEIGHT_MIN + vMargin * 2,
+                bottomHeight + (vMargin * 2) + heightOffset);
+        };
+    })(),
+
+    registerBeat: function (beatComponent) {
+        beatComponent.hitDetectors = this.weapons.map(weapon => {
+            if (beatComponent.data.type === 'mine')
+                var isWeaponCorrect = false;
+            else
+                var isWeaponCorrect = WEAPON_COLORS[weapon.el.dataset.hand] === beatComponent.data.color;
+            // if weaopo is a blade
+            if (this.data.gameMode === CLASSIC)
+                return new BladeHitDetector(weapon, beatComponent, isWeaponCorrect);
+            else 
+                return new PunchHitDetector(weapon, beatComponent, isWeaponCorrect);
+        });
+        this.beats.push(beatComponent);
+    },
+
+    unregisterBeat: function (beatComponent) {
+        this.beats.splice(this.beats.indexOf(beatComponent), 1);
+    }
+});
--- src/components/beat.js
+++ src/components/beat.js
@@ -1,45 +1,23 @@
-import { PUNCH_OFFSET, SWORD_OFFSET } from './beat-generator';
-import { HitChecker } from './hit-checker';
 
-const COLORS = require('../constants/colors.js');
-
-const auxObj3D = new THREE.Object3D();
-const bbox = new THREE.Box3();
-const otherBbox = new THREE.Box3();
-const collisionZThreshold = -1.65;
-
-const ANGLE_DOT_SUPER = 0.97; // ~15-degrees.
-const ANGLE_DOT_MIN = 0.625; // ~50-degrees.
 const WARMUP_TIME = 2000;
 const WARMUP_ROTATION_CHANGE = 2 * Math.PI;
 
 const elasticEasing = getElasticEasing(1.33, 0.5);
 
-const DESTROYED_SPEED = 1.0;
+
 const ONCE = { once: true };
 const DESTROY_TIME = 1000;
 
-// Play sound and explode at reach to test sync.
-const SYNC_TEST = !!AFRAME.utils.getUrlParameter('synctest');
-const syncTestObject3D = new THREE.Object3D();
-const syncTestVector3 = new THREE.Vector3();
+
 
 const MINE = 'mine';
-const CLASSIC = 'classic';
+const BEAT = 'beat';
 const DOT = 'dot';
+const CLASSIC = 'classic';
 const PUNCH = 'punch';
 const RIDE = 'ride';
 
-const CUT_DIRECTION_VECTORS = {
-  up: new THREE.Vector3(0, 1, 0),
-  down: new THREE.Vector3(0, -1, 0),
-  left: new THREE.Vector3(-1, 0, 0),
-  right: new THREE.Vector3(1, 0, 0),
-  upleft: new THREE.Vector3(-1, 1, 0).normalize(),
-  upright: new THREE.Vector3(1, 1, 0).normalize(),
-  downleft: new THREE.Vector3(-1, -1, 0).normalize(),
-  downright: new THREE.Vector3(1, -1, 0).normalize()
-};
+
 
 const MODELS = {
   arrowblue: 'blueBeatObjTemplate',
@@ -68,216 +46,6 @@ const SIZES = {
   [RIDE]: 0.4
 };
 
-AFRAME.registerComponent('beat-system', {
-  schema: {
-    gameMode: { default: 'classic', oneOf: ['classic', 'punch', 'ride'] },
-    hasVR: { default: false },
-    inVR: { default: false },
-    isLoading: { default: false },
-    isPlaying: { default: false }
-  },
-
-  init: function () {
-    this.beats = [];
-    this.beatsToCheck = [];
-    this.blades = [];
-    this.fists = [];
-    this.weapons = null;
-
-    this.bladeEls = this.el.sceneEl.querySelectorAll('a-entity[blade]');
-    this.curveFollowRig = document.getElementById('curveFollowRig');
-    this.punchEls = this.el.sceneEl.querySelectorAll('a-entity[punch]');
-    this.curveEl = document.getElementById('curve');
-    this.size = SIZES[this.data.gameMode];
-    this.supercurveFollow = null;
-  },
-
-  play: function () {
-    for (let i = 0; i < 2; i++) {
-      this.blades.push(this.bladeEls[i].components.blade);
-      this.fists.push(this.punchEls[i].components.punch);
-    }
-
-    this.supercurve = this.curveEl.components.supercurve;
-    this.supercurveFollow = this.curveFollowRig.components['supercurve-follow'];
-  },
-
-  update: function (oldData) {
-    this.size = SIZES[this.data.gameMode];
-
-    if (oldData.isLoading && !this.data.isLoading) {
-      this.updateBeatPositioning();
-      this.weaponOffset = this.data.gameMode === CLASSIC ? SWORD_OFFSET * 1.15 : PUNCH_OFFSET;
-      this.weaponOffset = this.weaponOffset / this.supercurve.curve.getLength();
-    }
-
-    if (oldData.gameMode !== this.data.gameMode) {
-      this.weapons = this.data.gameMode === CLASSIC ? this.blades : this.fists;
-    }
-  },
-
-  tick: function (t, dt) {
-    if (!this.data.isPlaying || this.data.gameMode === RIDE) { return; }
-
-    const beatsToCheck = this.beatsToCheck;
-    const curve = this.supercurve.curve;
-    const progress = this.supercurveFollow.songProgress;
-
-    // Filter for beats that should be checked for collisions.
-    beatsToCheck.length = 0;
-    for (let i = 0; i < this.beats.length; i++) {
-      const beat = this.beats[i];
-
-      // Check beat is not already destroyed.
-      if (beat.destroyed) { continue; }
-
-      // Check if beat is close enough to be hit.
-      const beatProgress = beat.songPosition - this.weaponOffset;
-      if (progress < beatProgress) { continue; }
-
-      // Check if beat should be filtered out due to not being in front.
-      let inFront = true;
-      for (let i = 0; i < beatsToCheck.length; i++) {
-        if (beat.horizontalPosition === beatsToCheck[i].horizontalPosition &&
-          beat.verticalPosition === beatsToCheck[i].verticalPosition &&
-          beat.songPosition > beatsToCheck[i].songPosition) {
-          inFront = false;
-        }
-        if (!inFront) { break; }
-      }
-      if (inFront) { beatsToCheck.push(beat); }
-    }
-
-    // Update bounding boxes and velocities.
-    this.weapons[0].tickBeatSystem(t, dt);
-    this.weapons[1].tickBeatSystem(t, dt);
-
-    // No beats to check means to collision to check.
-    if (!beatsToCheck.length) { return; }
-
-    // Check hits.
-    for (let i = 0; i < beatsToCheck.length; i++) {
-      // If ?synctest=true, auto-explode beat and play sound to easily test sync.
-      if ((SYNC_TEST || !this.data.hasVR) && beatsToCheck[i].data.type !== MINE) {
-        beatsToCheck[i].autoHit(this.weapons[0].el);
-        continue;
-      }
-      //this.checkCollision(beatsToCheck[i], this.weapons[0], this.weapons[1]);
-      beatsToCheck[i].hitCheckers.forEach(hitChecker => {
-        if (hitChecker.IsHit(t) && hitChecker.HitIsGood) {
-          beatsToCheck[i].onHit(hitChecker.blade.el);
-        }
-      });
-    }
-  },
-
-  checkCollision: function (beat, weapon1, weapon2) {
-    // Mine.
-    if (beat.data.type === MINE) {
-      if (weapon1.checkCollision(beat)) {
-        beat.onHit(weapon1.el);
-        return;
-      }
-      if (weapon2.checkCollision(beat)) {
-        beat.onHit(weapon2.el);
-      }
-      return;
-    }
-
-    // Successful hit, let the beat handle further processing.
-    const correctWeapon = WEAPON_COLORS[weapon1.el.dataset.hand] === beat.data.color
-      ? weapon1
-      : weapon2;
-    if (correctWeapon.checkCollision(beat)) {
-      beat.onHit(correctWeapon.el);
-      return;
-    }
-
-    // If not successful hit, check if mismatched hit.
-    const wrongWeapon = correctWeapon === weapon1 ? weapon2 : weapon1;
-    if (wrongWeapon.checkCollision(beat)) {
-      // Minimum speed for wrong beat.
-      if (this.data.gameMode === 'classic' && wrongWeapon.strokeSpeed < 15) {
-        return;
-      }
-
-      if (this.data.gameMode === 'punch' && wrongWeapon.speed < 2) {
-        return;
-      }
-
-      beat.onHit(wrongWeapon.el, true);
-      beat.destroyBeat(wrongWeapon.el, false);
-    }
-  },
-
-  horizontalPositions: {},
-
-  verticalPositions: {},
-
-  /**
-   * Update positioning between blocks, vertically and horizontally depending on
-   * game mode, and the height of the user.
-   *
-   * Adjustment revolves primary around SIZES, and the hMargin multiply factor.
-   */
-  updateBeatPositioning: (function () {
-    // Have punches be higher.
-    const BOTTOM_HEIGHTS = {
-      [CLASSIC]: 0.95,
-      [RIDE]: 0.95,
-      [PUNCH]: 1.20
-    };
-
-    const BOTTOM_HEIGHT_MIN = 0.4;
-    const REFERENCE_HEIGHT = 1.6;
-
-    return function () {
-      const gameMode = this.data.gameMode;
-      const horizontalPositions = this.horizontalPositions;
-      const verticalPositions = this.verticalPositions;
-
-      const heightOffset = this.el.sceneEl.camera.el.object3D.position.y - REFERENCE_HEIGHT;
-      const size = SIZES[gameMode];
-
-      // Horizontal margin based on size of blocks so they don't overlap, which a smidge
-      // of extra margin.
-      // For punch mode, we want a wider horizontal spread in punch range, but not vertical.
-      const hMargin = gameMode === CLASSIC ? size : size * 1.2;
-      horizontalPositions.left = -1.5 * hMargin;
-      horizontalPositions.middleleft = -0.5 * hMargin;
-      horizontalPositions.middle = hMargin;
-      horizontalPositions.middleright = 0.5 * hMargin;
-      horizontalPositions.right = 1.5 * hMargin;
-
-      // Vertical margin based on size of blocks so they don't overlap.
-      // And then overall shifted up and down based on user height (camera Y).
-      // But not too low to go underneath the ground.
-      const bottomHeight = BOTTOM_HEIGHTS[gameMode];
-      const vMargin = size;
-      verticalPositions.bottom = Math.max(
-        BOTTOM_HEIGHT_MIN,
-        bottomHeight + heightOffset);
-      verticalPositions.middle = Math.max(
-        BOTTOM_HEIGHT_MIN + vMargin,
-        bottomHeight + vMargin + heightOffset);
-      verticalPositions.top = Math.max(
-        BOTTOM_HEIGHT_MIN + vMargin * 2,
-        bottomHeight + (vMargin * 2) + heightOffset);
-    };
-  })(),
-
-  registerBeat: function (beatComponent) {
-    beatComponent.hitCheckers = this.blades.map(blade => {
-      return new HitChecker(blade, beatComponent, true);
-    });
-    this.beats.push(beatComponent);
-
-  },
-
-  unregisterBeat: function (beatComponent) {
-    this.beats.splice(this.beats.indexOf(beatComponent), 1);
-  }
-});
 
 /**
  * Bears, beats, Battlestar Galactica.
@@ -296,19 +64,13 @@ AFRAME.registerComponent('beat', {
     this.broken = null;
     this.brokenPoolName = undefined;
     this.destroyed = false;
-    this.hitEventDetail = {};
     this.poolName = undefined;
     this.returnToPoolTimer = DESTROY_TIME;
-    this.rotationAxis = new THREE.Vector3();
-    this.superCutIdx = 0;
-    this.startPositionZ = undefined;
-    this.warmupTime = 0;
-    this.weaponColors = { right: 'blue', left: 'red' };
+    this.warmupTime = 0;;
     this.curveEl = document.getElementById('curve');
     this.curveFollowRig = document.getElementById('curveFollowRig');
     this.mineParticles = document.getElementById('mineParticles');
     this.rigContainer = document.getElementById('rigContainer');
-    this.superCuts = document.querySelectorAll('.superCutFx');
     this.verticalPositions = this.beatSystem.verticalPositions;
 
 
@@ -328,18 +90,17 @@ AFRAME.registerComponent('beat', {
     this.el.appendChild(this.blockEl);
     this.initMesh();
 
+
     if (this.data.type === MINE) {
       this.poolName = 'pool__beat-mine';
     } else {
       this.poolName = `pool__beat-${this.data.type}-${this.data.color}`;
     }
+
     // for each beat let's create a plane to project the blade pointer to
     // on that plane is also project the beat bounding box ( as a rectangle )
     this.hitPlane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);  // it is a vertical plane that sits just behind the beat 
-    // for debug add a-plane to this entity
-    var plane = document.createElement('a-plane');
-    plane.setAttribute('scale', '0.29 0.29 0.29');
-    this.el.appendChild(plane);
+    
 
   },
 
@@ -347,14 +108,7 @@ AFRAME.registerComponent('beat', {
     const el = this.el;
     const data = this.data;
 
-    // Delay these events into next frame to spread out the workload.
-    if (this.queueBeatHitEvent) {
-      el.sceneEl.emit('beathit', this.queueBeatHitEvent, true);
-      this.queueBeatHitEvent = null;
-    } else if (this.queueBeatWrongEvent) {
-      el.sceneEl.emit('beatwrong', null, true);
-      this.queueBeatWrongEvent = null;
-    }
+
 
     if (this.destroyed) {
       this.returnToPoolTimer -= timeDelta;
@@ -370,14 +124,6 @@ AFRAME.registerComponent('beat', {
       this.warmupTime += timeDelta;
     }
 
-    // Check if past the camera to return to pool.
-    const returnDistance = this.data.type === 'mine' ? 0.25 : 1.25;
-    if ((el.object3D.position.z - returnDistance) > this.curveFollowRig.object3D.position.z) {
-      this.returnToPool();
-      this.missHit();
-    }
-
-
 
   },
 
@@ -387,10 +133,7 @@ AFRAME.registerComponent('beat', {
    */
   onGenerate: function (songPosition, horizontalPosition, verticalPosition, cutDirection, heightOffset) {
     const data = this.data;
-    const el = this.el;
-
-    this.beatSystem.registerBeat(this);
-
+    const el = this.el; 
     // Model is 0.29 size. We make it 1.0 so we can easily scale based on 1m size.
     const FACTOR = 1 / 0.29;
     const size = SIZES[this.beatSystem.data.gameMode] * FACTOR;
@@ -433,6 +176,8 @@ AFRAME.registerComponent('beat', {
     el.object3D.position.y -= offset;
     this.positionStart = el.object3D.position.y;
     this.positionChange = this.verticalPositions[verticalPosition] + offset + heightOffset;
+
+    this.beatSystem.registerBeat(this);
   },
 
   /**
@@ -456,17 +201,18 @@ AFRAME.registerComponent('beat', {
     if (this.data.type === 'mine') {
       this.bbox.expandByScalar(-0.25);
     }
+
+    // for debug add a-plane to this entity
+    //var plane = document.createElement('a-plane');
+    //plane.object3D.scale.set(this.bbox.max.x - this.bbox.min.x, this.bbox.max.y - this.bbox.min.y, 1);
+    //this.el.appendChild(plane);
   },
 
   wrongHit: function () {
     this.destroyed = true;
-    this.queueBeatWrongEvent = true;
   },
 
-  missHit: function () {
-    if (this.data.type === MINE) { return; }
-    this.el.sceneEl.emit('beatmiss', null, true);
-  },
+
 
   destroyBeat: function (weaponEl, correctHit) {
     const data = this.data;
@@ -505,9 +251,16 @@ AFRAME.registerComponent('beat', {
       weaponEl.components.trail.pulse();
     }
   },
-  isDot : function(){
+  isDot: function () {
     return this.data.type === DOT;
   },
+  isMine: function () {
+    return this.data.type === MINE;
+  },
+  isArrow: function () {
+    return this.data.type === BEAT;
+  },
+
   /**
    * Check if need to return to pool.
    */
@@ -516,144 +269,7 @@ AFRAME.registerComponent('beat', {
     this.el.object3D.position.set(0, 0, -9999);
     this.el.object3D.visible = false;
     this.el.sceneEl.components[this.poolName].returnEntity(this.el);
-  },
-
-  onHit: function (weaponEl, wrongHit) {
-    const data = this.data;
-
-    // Haptics.
-    try {
-      weaponEl.components.haptics__beat.pulse();
-    } catch (err) {
-      console.log(err);
-    }
-
-    // Sound.
-    this.el.parentNode.components['beat-hit-sound'].playSound(this.el, this.cutDirection);
-
-    if (wrongHit) {
-      this.wrongHit();
-      return;
-    }
-
-    if (data.type === MINE) {
-      this.destroyBeat(weaponEl, false);
-      this.el.sceneEl.emit('minehit', null, true);
-      return;
-    }
-
-    // Do blade-related checks.
-    if (this.beatSystem.data.gameMode === CLASSIC) {
-      let dot = 0;
-      if (data.type === 'arrow') {
-        const blade = weaponEl.components.blade;
-        dot = blade.strokeDirectionVector.dot(CUT_DIRECTION_VECTORS[this.cutDirection]);
-        if (dot < ANGLE_DOT_MIN) {
-          this.destroyBeat(weaponEl, false);
-          this.wrongHit();
-          return;
-        }
-      }
-      this.destroyBeat(weaponEl, true);
-      this.calculateScoreBlade(weaponEl, dot);
-      return;
-    }
-
-    // Do punch-related checks.
-    if (this.beatSystem.data.gameMode === PUNCH) {
-      this.destroyBeat(weaponEl, true);
-      this.calculateScorePunch(weaponEl);
-    }
-  },
-
-  /**
-   * Emit score and show score effects.
-   * Called by multiple modes (blade, punch).
-   */
-  score: function (score, percent) {
-    const el = this.el;
-    const hitEventDetail = this.hitEventDetail;
-
-    score = Math.ceil(parseInt(score, 10) / 10) * 10;
-    hitEventDetail.percent = percent;
-    hitEventDetail.score = score;
-    this.queueBeatHitEvent = hitEventDetail;
-
-    // Super FX.
-    if (percent >= 100) {
-      this.superCuts[this.superCutIdx].components.supercutfx.createSuperCut(
-        el.object3D, this.data.color);
-      this.superCutIdx = (this.superCutIdx + 1) % this.superCuts.length;
-    }
-  },
-
-  /**
-   * Blade scoring.
-   */
-  calculateScoreBlade: function (bladeEl, angleDot) {
-    const SUPER_SCORE_SPEED = 10;
-    const speed = bladeEl.components.blade.strokeSpeed;
-    const speedScore = (speed / SUPER_SCORE_SPEED) * 30;
-
-    let score;
-    if (speed <= SUPER_SCORE_SPEED) {
-      score = Math.min(speedScore, 30);
-    } else {
-      score = remap(clamp(speed, 10, 25), 10, 25, 30, 50);
-    }
-
-    let percent = Math.min(speedScore, 30);
-
-    // 70% score on direction.
-    if (this.data.type === DOT) {
-      score += 70;
-      percent += 70;
-    } else {
-      score += angleDot * 70;
-      if (angleDot > ANGLE_DOT_SUPER) {
-        percent += 70;
-      } else {
-        percent += angleDot * 70;
-      }
-    }
-
-    this.score(score, percent);
-  },
-
-  /**
-   * A possible To Do would be to score based on punch velocity and direction.
-   * More points scored if punching straight down the curve.
-   */
-  calculateScorePunch: function (punchEl) {
-    const base = 60; // Get 60% of the score just by hitting the beat.
-
-    const SUPER_SCORE_SPEED = 1.5;
-    const speed = punchEl.components.punch.speed;
-    const speedScore = (speed / SUPER_SCORE_SPEED) * 40;
-
-    let score;
-    if (speed <= SUPER_SCORE_SPEED) {
-      score = base + Math.min(speedScore, 40);
-    } else {
-      score = base + remap(clamp(speed, 1.5, 6), 1.5, 6, 40, 70);
-    }
-
-    const percent = base + Math.min(speedScore, 40);
-    this.score(score, percent);
-  },
-
-  /**
-   * Play hit if SYNC_TEST or 2D viewer mode.
-   */
-  autoHit: function (weaponEl) {
-    const el = this.el;
-    this.destroyBeat(weaponEl, Math.random() < 0.9);
-    el.parentNode.components['beat-hit-sound'].playSound(el, this.cutDirection);
-    const hitEventDetail = this.hitEventDetail;
-    hitEventDetail.percent = 100;
-    hitEventDetail.score = 100;
-    this.queueBeatHitEvent = hitEventDetail;
-  }
+  },  
 });
 
 /**
